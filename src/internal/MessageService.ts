3de9897a24364f17ef20bbfb417326df9b503b03
myungjaeyu <rootcode10@gmail.com>
2018-10-29 12:14:58 +0900

    Added setName() and talk() methods.

--- src/internal/MessageService.ts
+++ src/internal/MessageService.ts
@@ -8,5 +8,22 @@ import { HashMap } from 'tstl/container/HashMap'
 export default class MessageService implements IMessageService {
 
     private static users : HashMap<string, Driver<IMessagePrinter>> = new HashMap()
+    private driver : Driver<IMessagePrinter>
+    private name : string
+
+    public constructor(driver : Driver<IMessagePrinter>) {
+
+        this.driver = driver
+
+    }
+
+    public setName(name : string): boolean {
+
+		return true
+    }
+
+    public talk(content : string): void {
+
+    }
 
 }
