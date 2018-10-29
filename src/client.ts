08ce88f0ac758640fa4ba90e48f14a6717b6c291
myungjaeyu <rootcode10@gmail.com>
2018-10-29 12:33:44 +0900

    Added readline function.

--- src/client.ts
+++ src/client.ts
@@ -4,6 +4,22 @@ import { Driver } from 'tgrid/base'
 import IMessageService from './internal/interface/IMessageService'
 import MessagePrinter from './internal/MessagePrinter'
 
+import { createInterface } from 'readline'
+
+
+const _read = (question: string): Promise<string> => 
+    new Promise(resolve => {
+
+        let readline = createInterface({ input: process.stdin, output: process.stdout })
+
+        readline.question(question, input => {
+            resolve(input)
+
+            readline.close()
+        })
+
+    })
+
 async function main() : Promise<void> {
 
     const connector = new WebConnector(new MessagePrinter())
@@ -11,6 +27,14 @@ async function main() : Promise<void> {
 
 	const messageService : Driver<IMessageService> = connector.getDriver<IMessageService>()
     let name : string
+
+
+	while (true) {
+
+        name = await _read('insert name : ')
+
+        if (await messageService.setName(name) === true) break
+	}
 
     await connector.close()
 
