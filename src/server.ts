6c68bd80e12910283e61b64802a33bd01c5a49ab
myungjaeyu <rootcode10@gmail.com>
2018-10-29 12:41:23 +0900

    Added server thread.

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,16 @@
-import { WebServer } from 'tgrid/protocol/web'
+import { WebServer, WebAcceptor } from 'tgrid/protocol/web'
+
+import MessageService from './internal/MessageService'
 
 async function main() : Promise<void> {
 
     const server = new WebServer()
+
+    server.open(10101, async (acceptor : WebAcceptor) => {
+
+        await acceptor.accept()
+        await acceptor.listen(new MessageService(acceptor.getDriver()))
+    })
 
 }
 
